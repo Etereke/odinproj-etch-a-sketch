6b321a60e937da279de089e158c12168382e1753
Nagy-Zsugya Martin <98815314+Etereke@users.noreply.github.com>
2024-02-02 19:44:52 +0100

    Implement color picking and color darkening User can now pick a color with the color-picker on the right. When hovering over a pixel that you have already hovered over with the same color, the pixel gets 10% darker (compared to the original color, so it gets completely black after 10 hovers).

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
 const canvas = document.querySelector('.canvas');
 const buttonPixel = document.querySelector('.button-pixel');
 const buttonColor = document.querySelector('.button-color');
-
-// const canvasWidth = canvas.clientWidth;
-// const canvasHeight = canvas.clientHeight;
-// const canvasPixelWidth = canvas.clientWidth / resolution + 'px';
-// const canvasPixelHeight = canvas.clientHeight / resolution + 'px';
-
-// const baseColor = '#eeeceb';
-// const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-// let currentRes = 16;
-let color = 'black';
-function createCanvas(resolution) {
-    const divList = document.querySelectorAll('.canvas > div');
-    const canvasPixelWidth = canvas.clientWidth / resolution + 'px';
-    const canvasPixelHeight = canvas.clientHeight / resolution + 'px';
-    divList.forEach((div) => {
-        // div.removeEventListener()
-        div.remove();
-    });
-    // currentRes = resolution;
-    for (let i = 0; i < resolution * resolution; i++) {
-        const div = document.createElement('div');
-        div.style.width = canvasPixelWidth;
-        div.style.height = canvasPixelHeight;
-        // div.style.flex = `1 0 ${canvasPixelWidth}`;
-        div.addEventListener('mouseenter', handleMouse);
-        canvas.appendChild(div);
-    }
-
-}
+let color = buttonColor.value;
 
 buttonPixel.addEventListener('click', () => {
     let res = 0;
@@ -38,9 +10,59 @@ buttonPixel.addEventListener('click', () => {
     }
     createCanvas(res);
 });
-
-function handleMouse(event) {
-    event.target.style.backgroundColor = color;
-}
+buttonColor.addEventListener('input', (e) => {
+    color = e.target.value;
+});
 
 createCanvas(16);
+
+function createCanvas(resolution) {
+    const divList = document.querySelectorAll('.canvas > div');
+    const canvasPixelWidth = canvas.clientWidth / resolution + 'px';
+    const canvasPixelHeight = canvas.clientHeight / resolution + 'px';
+    divList.forEach((div) => {
+        div.remove();
+    });
+    for (let i = 0; i < resolution * resolution; i++) {
+        const div = document.createElement('div');
+        div.style.width = canvasPixelWidth;
+        div.style.height = canvasPixelHeight;
+        div["data-color"] = "";
+        div.addEventListener('mouseenter', handleMouse);
+        canvas.appendChild(div);
+    }
+}
+
+function makeColorDarker(targetColor, originalColor) {
+    //originalColor is HEX format
+    let r = parseInt(originalColor.substr(1,2), 16);
+    let g = parseInt(originalColor.substr(3,2), 16);
+    let b = parseInt(originalColor.substr(5,2), 16);
+
+    //targetColor is 'rgb(x, x, x)' format
+    let colorArray = targetColor.split(",");
+    let newR = parseInt(colorArray[0].replace('rgb', '').replace('a', '').replace('(', ''));
+    let newG = parseInt(colorArray[1].trim());
+    let newB = parseInt(colorArray[2].trim().replace(')', ''));
+    
+    newR = newR - (r / 10) > 0
+            ? Math.floor(newR - (r / 10))
+            : 0;
+    newG = newG - (g / 10) > 0
+            ? Math.floor(newG - (g / 10))
+            : 0;
+    newB = newB - (b / 10) > 0
+            ? Math.floor(newB - (b / 10))
+            : 0;
+
+    return '#' + newR.toString(16).padStart(2, '0')  + newG.toString(16).padStart(2, '0')  + newB.toString(16).padStart(2, '0')
+}
+
+function handleMouse(event) {
+    if (event.target["data-color"] !== color) {
+        event.target["data-color"] = color;
+        event.target.style.backgroundColor = color;
+    } else {
+        event.target.style.backgroundColor = makeColorDarker(event.target.style.backgroundColor, color);
+    }
+}
